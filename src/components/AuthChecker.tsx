831751e884db37805bd0307a85d624e7b86458bb
Dmytro Kyrychyk <maestrokd.reg@gmail.com>
2025-06-19 00:20:27 -0700

    feat: GH-13 - Login Test 3

--- src/components/AuthChecker.tsx
+++ src/components/AuthChecker.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import {Outlet, useLocation, useNavigate} from 'react-router-dom';
 import {useAuth} from '../contexts/AuthContext';
+import WebApp from "@twa-dev/sdk";
+import {loginWithTelegram} from "@/services/AuthService.ts";
 
 const AuthChecker: React.FC = () => {
   const {telegramAuth, addTelegramAuth} = useAuth();
@@ -9,9 +11,28 @@ const AuthChecker: React.FC = () => {
 
   useEffect(() => {
     console.log('AuthChecker - in ');
-    /*if (telegramAuth) {
-      navigate(pathname);
-    }*/
+
+    const authenticate = async () => {
+
+      if (WebApp) {
+        WebApp.ready();
+        const initDataString = WebApp.initData;
+        console.log('initDataString', initDataString);
+
+        if (initDataString) {
+          try {
+            await loginWithTelegram(initDataString);
+          } catch (err) {
+            console.error('Telegram login failed', err);
+            /*navigate('/login', {replace: true});
+            return;*/
+          }
+        }
+      }
+    }
+
+    authenticate();
+
     const params = new URLSearchParams(search);
     const authParam = params.get('telegram_auth') || undefined;
     if (authParam) {
