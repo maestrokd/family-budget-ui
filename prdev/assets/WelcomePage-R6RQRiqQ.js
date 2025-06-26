61178e8583e8c0bedc94b929f1e27727e9e6f16f
maestrokd <maestrokd@users.noreply.github.com>
2025-06-26 00:20:20 +0000

    chore: Deploy to PR-DEV

--- prdev/assets/WelcomePage-R6RQRiqQ.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-BqlojyIY.js";const s=()=>e.jsx("div",{className:"flex items-center justify-center h-full p-6",children:e.jsx("h2",{className:"text-2xl font-semibold text-gray-800",children:"Welcome to the Service Dashboard"})});export{s as default};
